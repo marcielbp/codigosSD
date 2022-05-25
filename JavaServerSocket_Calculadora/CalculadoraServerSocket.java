d80f09c8f2ad8fb4bc0ab5432619459ebb0bfe0f
Marciel Barros <marcielbp@gmail.com>
2022-05-25 17:36:04 -0300

    Add files via upload
    Update socket server for Calculadora Class

--- JavaServerSocket_Calculadora/CalculadoraServerSocket.java
+++ JavaServerSocket_Calculadora/CalculadoraServerSocket.java
@@ -19,23 +19,39 @@ public class CalculadoraServerSocket {
 	    Calculadora calc = new Calculadora();
 		try {
 			welcomeSocket = new ServerSocket(9090);
-		  int i=0; //número de clientes
+		  int i=0; //num clientes
 	  
 	      System.out.println ("Servidor no ar");
 	      while(true) { 
 	  
 	           Socket connectionSocket = welcomeSocket.accept(); 
 	           i++;
-	           System.out.println ("Nova conexão");
+	           System.out.println ("Nova conexao");
 	           
 	           //Interpretando dados do servidor
 	           socketEntrada = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                String operacao= socketEntrada.readLine();
                String oper1=socketEntrada.readLine();
                String oper2=socketEntrada.readLine();
-               
+               String result = "";
                //Chamando a calculadora
-               String result= ""+calc.soma(Double.parseDouble(oper1),Double.parseDouble(oper2));
+               switch(operacao) {
+	               case "1": // soma
+	            	   result= ""+calc.soma(Double.parseDouble(oper1),Double.parseDouble(oper2));
+	            	   break;
+	               case "2": // subtracao
+	            	   result= ""+calc.subtracao(Double.parseDouble(oper1),Double.parseDouble(oper2));
+	            	   break;
+	               case "3": // multiplicacao
+	            	   result= ""+calc.multiplicacao(Double.parseDouble(oper1),Double.parseDouble(oper2));
+	            	   break;
+	               case "4": // divisao
+	            	   result= ""+calc.divisao(Double.parseDouble(oper1),Double.parseDouble(oper2));
+	            	   break;
+	               default:
+	            	   result= "operacao inexistente";
+	             }
+               
                
                //Enviando dados para o servidor
                socketOutput= new DataOutputStream(connectionSocket.getOutputStream());     	
@@ -43,8 +59,7 @@ public class CalculadoraServerSocket {
 	           System.out.println (result);	           
 	           socketOutput.flush();
 	           socketOutput.close();
-
-	                    
+          
 	      }
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
