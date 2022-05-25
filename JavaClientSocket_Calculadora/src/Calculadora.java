6a4b35a443cb9087b98ba850972d1aa4dee99653
Marciel Barros <marcielbp@gmail.com>
2022-05-25 17:35:20 -0300

    Add files via upload
    Update client Calculadora class and main file

--- JavaClientSocket_Calculadora/src/Calculadora.java
+++ JavaClientSocket_Calculadora/src/Calculadora.java
@@ -5,4 +5,16 @@ public class Calculadora {
     public double soma(double oper1, double oper2) {
         return oper1 + oper2;
     }
+    public double subtracao(double oper1, double oper2) {
+        return oper1 - oper2;
+    }
+    public double multiplicacao(double oper1, double oper2) {
+        return oper1 * oper2;
+    }
+    public double divisao(double oper1, double oper2) {
+    	if ((oper2 < 0)||(oper2 > 0)){
+    		return oper1 / oper2;
+    	}
+        	return Float.NaN;
+    }
 }
--- JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
+++ JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
@@ -9,13 +9,14 @@ public class CalculadoraClientSocket {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		double oper1=10,oper2=20;
-		int operacao=1; //1-somar 2-subtrair 3-dividir 4-multiplicar
+		double oper1=10,oper2=0;
+		int operacao=4; //1-somar 2-subtrair 3-dividir 4-multiplicar
 		String result="";
         try {
 
-        	//Conexão com o Servidor
-            Socket clientSocket = new Socket("192.168.0.11", 9090);
+        	//Conexao com o Servidor
+            // Socket clientSocket = new Socket("192.168.0.11", 9090);
+        	Socket clientSocket = new Socket("192.168.1.14", 9090);
             DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream());
             
             //Enviando os dados
